faba5efb41e2df628a2f3f0b0f3dae479b53fa29
AlexSath <sathler10@gmail.com>
2021-05-18 21:24:51 +0000

    added .gitignore and paramaterized hello func

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def saying_hello():
     print("just wanted to say hello to my colaborator")
 
-def hello_riely():
-    print("Hey Riely good to work with you!")
+def hello(name):
+    print(f"Hello {name}, pleasure working with you!")
     
