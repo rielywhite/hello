dc03e61e3d79808b78c9f1597af7d4162d7495ef
AlexSath <sathler10@gmail.com>
2021-05-18 21:52:45 +0000

    added 'second hello'

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def saying_hello():
     print("just wanted to say hello to my colaborator")
+    print("Why not say hello again?")
 
 def hello(name):
     print(f"Hello {name}, pleasure working with you!")
