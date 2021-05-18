61b443578cfde31ad2b20b3fa9f4aa6cfdcf1a62
AlexSath <sathler10@gmail.com>
2021-05-18 21:54:03 +0000

    moved conflict line into first func

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def saying_hello():
     print("just wanted to say hello to my colaborator")
     print("Why not say hello again?")
+    print("making a commit to make conflict")
 
 def hello(name):
     print(f"Hello {name}, pleasure working with you!")
-print("making a commit to make conflict")
 
